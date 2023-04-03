4621cc8a03e7e4a94cb888b8974c72b0478f0af5
mkropidlowski <mkropidlowsky@gmail.com>
2023-04-03 21:12:52 +0200

    SHOP-1: Create HERO section + mobile

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image';
 import styles from './hero.module.scss';
+import iphoneBanner from '../icons/iphone-14.png';
 
 const Hero = () => {
-	return <div className={styles.wrapper}>HERO SECTION</div>;
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.content}>
+				<div className={styles.text}>
+					<h1 className={styles.header}>SALES</h1>
+					<p className={styles.description}>
+						Convallis interdum purus adipiscing dis parturient posuere ac a quam a eleifend montes parturient posuere curae
+						tempor
+					</p>
+					<div className={styles.buttons}>
+						<button>Read more</button>
+						<button>Shop now!</button>
+					</div>
+				</div>
+				<Image className={styles.bannerLogo} src={iphoneBanner} alt="Iphone 14 PRO" />
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
